c00d83733132c32fe9ee0afa4f92d69162a643c9
Gonzalo Hernández <gonzalohernandez@icloud.com>
2019-11-29 17:47:38 +0100

    refactor

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
+window.onload = setInterval(() => {
+  draw();
+}, 10);
+
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var rightPressed = false;
 var leftPressed = false;
+
+// -----------------------
+// CLASSES AND OBJECTS
+// -----------------------
 
 class Character {
   constructor(x, y, width, height, image, sprites) {
@@ -21,22 +29,82 @@ class Character {
   }
 }
 
+class Hero extends Character {
+  constructor(x, y, width, height, image, sprites) {
+    super(x, y, width, height, image, sprites);
+  }
+}
+
+class Enemey extends Character {
+  constructor(x, y, width, height, image, sprites) {
+    super(x, y, width, height, image, sprites);
+    this.walk();
+  }
+  walk() {
+    setInterval(() => {
+      if (!this.isFlying) {
+        this.x += this.speed;
+        this.activeSprite++;
+        if (this.activeSprite === this.sprites) this.activeSprite = 0;
+        if (collision(this)) this.speed = -this.speed;
+        if (!this.isRotating) {
+          if (this.speed > 0) {
+            this.isLookingLeft = this.gravityAcc > 0 ? 0 : 1;
+          } else {
+            this.isLookingLeft = this.gravityAcc < 0 ? 0 : 1;
+          }
+        }
+      }
+    }, 20);
+  }
+}
+
 // characters creation
-var hero = new Character(300, 100, 46, 60, "hero-sprites.png", 24);
+var hero = new Hero(300, 100, 46, 60, "./img/hero-sprites.png", 24);
 var enemies = [];
-enemies.push(new Character(550, 100, 46, 60, "hero-sprites.png"));
-enemies.push(new Character(150, 100, 46, 60, "hero-sprites.png"));
+enemies.push(new Enemey(550, 100, 44.5, 60, "./img/enemy-sprites.png", 24));
+enemies.push(new Enemey(150, 100, 44.5, 60, "./img/enemy-sprites.png", 24));
+
+// -----------------------
+// DRAWING
+// -----------------------
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   keyboard(hero);
-  enemies.forEach(element => {
-    drawCharacter(element);
+  enemies.forEach(enemy => {
+    drawCharacter(enemy);
   });
   drawCharacter(hero);
 }
 
 function drawCharacter(character) {
+  rotation(character);
+  gravity(character);
+  collision(character);
+
+  if (character.activeSprite === character.sprites) character.activeSprite = 0;
+  let sx = character.w * character.activeSprite;
+  let sy = character.h * character.isLookingLeft;
+  ctx.drawImage(
+    character.image,
+    sx,
+    sy,
+    character.w,
+    character.h,
+    character.x,
+    character.y,
+    character.w,
+    character.h
+  );
+  ctx.restore();
+}
+
+// -----------------------
+// PHYSICS
+// -----------------------
+
+function rotation(character) {
   // rotate when gravity changes
   if (character.isRotating) {
     if (character.gravityAcc > -6 && character.gravityAcc < 0) {
@@ -67,25 +135,6 @@ function drawCharacter(character) {
     ctx.rotate((Math.PI / 180) * degrees);
     ctx.translate(-Math.abs(xTranslate), -Math.abs(yTranslate));
   }
-
-  gravity(character);
-  collision(character);
-
-  if (character.activeSprite === character.sprites) character.activeSprite = 0;
-  let sx = character.w * character.activeSprite;
-  let sy = character.h * character.isLookingLeft;
-  ctx.drawImage(
-    character.image,
-    sx,
-    sy,
-    character.w,
-    character.h,
-    character.x,
-    character.y,
-    character.w,
-    character.h
-  );
-  ctx.restore();
 }
 
 function gravity(character) {
@@ -102,6 +151,8 @@ function gravity(character) {
 }
 
 function collision(character) {
+  let sideCollision = false; // for enemies
+
   // check if the character is colliding with the floor
   if (character.y > canvas.height - character.h) {
     character.y = canvas.height - character.h;
@@ -119,13 +170,21 @@ function collision(character) {
   // check if the character is colliding with the left side
   if (character.x < 0) {
     character.x = 0;
+    sideCollision = true;
   }
 
   // check if the character is colliding with the right side
   if (character.x > canvas.width - character.w) {
     character.x = canvas.width - character.w;
+    sideCollision = true;
   }
+
+  return sideCollision;
 }
+
+// -----------------------
+// KEYBOARD CONTROL
+// -----------------------
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -135,14 +194,15 @@ function keyDownHandler(e) {
     rightPressed = true;
   } else if (e.key == "Left" || e.key == "ArrowLeft") {
     leftPressed = true;
-  } else if (e.key == "z" && !hero.isFlying) {
+  } else if ((e.key == "z" || e.key == "Z") && !hero.isFlying) {
     hero.gravityAcc = -hero.gravityAcc;
     hero.isFlying = true;
     hero.isRotating = true;
-    enemies.forEach(element => {
-      element.gravityAcc = -Math.sign(element.gravityAcc);
-      element.isFlying = true;
-      element.isRotating = true;
+    enemies.forEach(enemy => {
+      enemy.gravityAcc = -Math.sign(enemy.gravityAcc);
+      enemy.isFlying = true;
+      enemy.isRotating = true;
+      enemy.speed = -enemy.speed;
     });
   }
 }
@@ -171,7 +231,3 @@ function keyboard(character) {
   }
   collision(character);
 }
-
-setInterval(() => {
-  draw();
-}, 10);
